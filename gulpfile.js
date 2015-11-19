393acedb8b12dfab65299321197c6c947002ddb5
anthonyherve <anthony.herve28@gmail.com>
2015-11-19 13:26:14 +0100

    Add gulp dependencies and edit some gulp functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,68 @@
-var gulp = require('gulp');
+var gulp = require('gulp-param')(require('gulp'), process.argv);
 var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
 
-gulp.task('default', function () {});
+gulp.task('default', function () {
+});
 
 var libPath = '/app/Resources/lib/';
 
-// Create task to compile scaa files to css
+// Create task to compile scss files to css
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
-gulp.task('sass', function () {
-    gulp.src('./web/bundles/common/sass/main.scss')
-        .pipe(sass({sourceComments: 'map', errLogToConsole: true}))
-        .pipe(rename('main.css'))
-        .pipe(minifyCss())
-        .pipe(gulp.dest('./web/css/'));
+gulp.task('compile-sass', function (env) {
+    if (env == 'prod') {
+        gulp.src('./web/bundles/common/sass/main.scss')
+            .pipe(plumber({errorHandler: onError}))
+            .pipe(sass({errLogToConsole: true}))
+            .pipe(rename('main.css'))
+            .pipe(minifyCss({rebase: false}))
+            .pipe(gulp.dest('./web/css/'));
+    } else {
+        gulp.src('./web/bundles/common/sass/main.scss')
+            .pipe(plumber({errorHandler: onError}))
+            .pipe(sass({errLogToConsole: true}))
+            .pipe(rename('main.css'))
+            .pipe(gulp.dest('./web/css/'));
+    }
 });
 
 
 // Create task to copy fonts to web/fonts
 var copy = copy = require('gulp-copy');
 gulp.task('fonts', function () {
+    gulp.src(['./web/bundles/*/fonts/**/*']).pipe(copy('./web/fonts', {prefix: 4}));
     return gulp.src('.' + libPath + 'bootstrap-sass/assets/fonts/bootstrap/*')
-        .pipe(copy('./web/fonts', {prefix: 7}));
+        .pipe(copy('./web/fonts', {prefix: 6}));
 });
 
 gulp.task('img', function () {
-    return gulp.src(['./web/bundles/*/img/**/*','./web/bundles/*/imgs/**/*','./web/bundles/*/images/**/*'])
-        .pipe(copy('./web/img', {prefix: 7}));
+    return gulp.src(['./web/bundles/*/img/**/*', './web/bundles/*/imgs/**/*', './web/bundles/*/images/**/*'])
+        .pipe(copy('./web/img', {prefix: 4}));
 });
 
 
 // Create task to copy js on web/js
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-gulp.task('js', function() {
-    gulp.src([
-        './web/bundles/*/js/**/*.js',
-        '.' + libPath + 'bootstrap-sass/assets/javascripts/bootstrap.min.js',
-        '.' + libPath + 'jquery/dist/jquery.min.js'
-    ])
-        //.pipe(concat('main.js'))
-        //.pipe(uglify())
-        .pipe(gulp.dest('./web/js'));
+gulp.task('compile-js', function (env) {
+    if (env == 'prod') {
+        gulp.src([
+                './web/bundles/*/js/**/*.js',
+                '.' + libPath + 'bootstrap-sass/assets/javascripts/bootstrap.min.js',
+                '.' + libPath + 'jquery/dist/jquery.min.js'
+            ])
+            .pipe(concat('main.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('./web/js'));
+    } else {
+        gulp.src([
+                './web/bundles/*/js/**/*.js',
+                '.' + libPath + 'bootstrap-sass/assets/javascripts/bootstrap.min.js',
+                '.' + libPath + 'jquery/dist/jquery.min.js'
+            ])
+            .pipe(gulp.dest('./web/js'));
+    }
 });
 
 
@@ -58,7 +79,7 @@ gulp.task('coverage', function () {
     return gulp.src('./src/*/Tests/**/*.php')
         .pipe(phpunit(
             './bin/phpunit',
-            {debug: false, configurationFile: './app/phpunit.xml', coverageHtml: './web/build/coverage'}
+            {debug: true, configurationFile: './app/phpunit.xml', coverageHtml: './web/build/coverage'}
         ));
 });
 
@@ -83,16 +104,23 @@ gulp.task('installAssets', function () {
 });
 
 
-gulp.task('compile-sass', ['installAssets', 'sass']);
-gulp.task('compile-js', ['installAssets', 'js']);
+gulp.task('sass', ['installAssets', 'compile-sass']);
+gulp.task('js', ['installAssets', 'compile-js']);
 // Create task to watch changes on js and scss
 gulp.task('watch', function () {
     var onChange = function (event) {
-        console.log('File '+event.path+' has been '+event.type);
+        console.log('File ' + event.path + ' has been ' + event.type);
     };
-    gulp.watch('./src/*/Resources/public/sass/**/*.scss', ['compile-sass'])
+    // Install assets when adding or deleting a file
+    gulp.watch('./src/**/Resources/public/**/*', ['installAssets'])
+        .on('add', onChange);
+    gulp.watch('./src/**/Resources/public/**/*', ['installAssets'])
+        .on('unlink', onChange);
+    // Compile SASS when changing scss files
+    gulp.watch('./src/**/Resources/public/sass/**/*.scss', ['sass'])
         .on('change', onChange);
-    gulp.watch('./src/*/Resources/public/js/**/*.js', ['compile-js'])
+    // Compile JS when changing js files
+    gulp.watch('./src/**/Resources/public/js/**/*.js', ['compile-js'])
         .on('change', onChange);
 });
 
@@ -100,4 +128,8 @@ gulp.task('watch', function () {
 // Without this function exec() will not show any output
 var logStdOutAndErr = function (err, stdout, stderr) {
     console.log(stdout + stderr);
+};
+
+var onError = function (err) {
+    console.log(err);
 };
